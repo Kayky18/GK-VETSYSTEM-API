03046dc8ea214cb166fa822329c79cbdc0b063ea
Kayky <kayky.marinho18@gmail.com>
2024-09-24 11:21:15 -0300

    feat: add UpdatePatientsFieldsChanged

--- handler/request.go
+++ handler/request.go
@@ -46,6 +46,40 @@ func (r *CreatePatientsRequest) ToSchema() *schemas.Patients {
 	}
 }
 
+func (request *UpdatePatientsRequest) UpdatePatientsFieldsChanged(patients *schemas.Patients) *schemas.Patients {
+	if request.NameTutor != "" {
+		patients.NameTutor = request.NameTutor
+	}
+	if request.Age > 0 {
+		patients.Age = request.Age
+	}
+	if request.Breed != "" {
+		patients.Breed = request.Breed
+	}
+	if request.CPF != "" {
+		patients.CPF = request.CPF
+	}
+	if request.Name != "" {
+		patients.Name = request.Name
+	}
+	if request.Phone != "" {
+		patients.Phone = request.Phone
+	}
+	if request.State != "" {
+		patients.State = request.State
+	}
+	if request.Temperature > 0 {
+		patients.Temperature = request.Temperature
+	}
+	if request.Weight > 0 {
+		patients.Weight = request.Weight
+	}
+	if request.Species != "" {
+		patients.Species = request.Species
+	}
+	return patients
+}
+
 func (request *UpdatePatientsRequest) Validate() error {
 	if request.NameTutor != "" || request.Age > 0 || request.Breed != "" || request.CPF != "" || request.Name != "" || request.Phone != "" || request.State != "" || request.Temperature > 0 || request.Weight > 0 || request.Species != "" {
 		return nil
--- handler/updatePatientsHandler.go
+++ handler/updatePatientsHandler.go
@@ -18,20 +18,24 @@ import (
 // @Param request body UpdatePatientsRequest true "Request Body"
 // @Success 200 {object} UpdatePatientsReponse
 // @Failure 400  {object} ErrorResponse
+// @Failure 404  {object} ErrorResponse
 // @Router /patient/{id} [put]
 func UpdatePatients(ctx *gin.Context) {
 	// Get the patient ID from the URL parameter
 	id := ctx.Param("id")
 
 	if id == "" {
-		sendError(ctx, http.StatusBadRequest, "id is required")
+		sendError(ctx, http.StatusBadRequest, "id-is-required")
 		return
 	}
 
 	request := UpdatePatientsRequest{}
-	ctx.BindJSON(&request)
+	err := ctx.ShouldBindJSON(&request)
+	if err != nil {
+		sendError(ctx, http.StatusBadRequest, "invalid-request")
+	}
 
-	err := request.Validate()
+	err = request.Validate()
 	if err != nil {
 		sendError(ctx, http.StatusBadRequest, err.Error())
 		return
@@ -39,45 +43,16 @@ func UpdatePatients(ctx *gin.Context) {
 
 	patients := schemas.GetSchema()
 
-	if err := db.First(&patients, id).Error; err != nil {
-		sendError(ctx, http.StatusBadRequest, "id not found")
+	if err := db.WithContext(ctx.Request.Context()).First(&patients, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "id-not-found")
 		return
 	}
 
-	if request.NameTutor != "" {
-		patients.NameTutor = request.NameTutor
-	}
-	if request.Age > 0 {
-		patients.Age = request.Age
-	}
-	if request.Breed != "" {
-		patients.Breed = request.Breed
-	}
-	if request.CPF != "" {
-		patients.CPF = request.CPF
-	}
-	if request.Name != "" {
-		patients.Name = request.Name
-	}
-	if request.Phone != "" {
-		patients.Phone = request.Phone
-	}
-	if request.State != "" {
-		patients.State = request.State
-	}
-	if request.Temperature > 0 {
-		patients.Temperature = request.Temperature
-	}
-	if request.Weight > 0 {
-		patients.Weight = request.Weight
-	}
-	if request.Species != "" {
-		patients.Species = request.Species
-	}
+	patients = request.UpdatePatientsFieldsChanged(patients)
 
-	if err := db.Save(&patients).Error; err != nil {
+	if err := db.WithContext(ctx.Request.Context()).Save(&patients).Error; err != nil {
 		sendError(ctx, http.StatusInternalServerError, "InternalServerError")
 		return
 	}
-	sendSucess(ctx, "sucess-put", patients)
+	sendSucess(ctx, "sucess-update-patient", patients)
 }
